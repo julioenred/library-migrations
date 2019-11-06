a18d20be6af0612eaef2619f2c7f19a6c7efbf47
juliocev <julioperezdecastro@cev.com>
2019-11-06 13:20:25 +0100

    refactor migrations

--- database/migrations/2019_11_06_121711_create_users_table.php
+++ database/migrations/2019_11_06_121711_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersLendBooksTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersLendBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_lend_books', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
-            $table->unsignedInteger('book_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateUsersLendBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_lend_books');
+        Schema::dropIfExists('users');
     }
 }
--- database/migrations/2019_11_06_121813_create_user_lend_books_table.php
+++ database/migrations/2019_11_06_121813_create_user_lend_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBooksTable extends Migration
+class CreateUserLendBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('user_lend_books', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->string('description');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('user_lend_books');
     }
 }
