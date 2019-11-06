0e041f59a08bd5af298a20afc02141b5f2fbc954
juliocev <julioperezdecastro@cev.com>
2019-11-06 13:59:32 +0100

    create model book

--- app/Book.php
+++ app/Book.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $fillable = [
-        'title', 'description',
-    ];
+    protected $table = "books";
 
-    public function users()
-    {
-        return $this->belongsToMany('App\User');
-    }
+    protected $fillable = ['title', 'description'];
+
+    // public function users()
+    // {
+    //     return $this->belongsToMany('App\User');
+    // }
+
 }
